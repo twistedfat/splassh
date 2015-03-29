afb1166123bfc7bb69c838864bc850cadcbed99a
Salim D <sdabdoub6@gatech.edu>
2015-03-29 14:05:01 -0400

    hi

--- client/views/ripple/ripple.js
+++ client/views/ripple/ripple.js
@@ -1,4 +1,4 @@
-Template.theripple.projects = function() {
+Template.theripple.helpers = function() {
   // get the top 5 recently added projects
   return Projects.find({}, {sort: { date_created: -1}, limit: 4});//.sort({_id: -1}).limit(5);
 }
