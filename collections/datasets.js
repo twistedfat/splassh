60ba604ecbf7c361072923fba0d9523ff69542bc
SAMSON <splasshorg@gmail.com>
2015-06-03 14:52:49 -0400

    cherrypicked

---
+++ collections/datasets.js
@@ -0,0 +1,39 @@
+/* Primary data collection */
+Datasets = new Meteor.Collection("datasets");
+
+Datasets.allow({
+    insert: function (userId, dataset) {
+      return true;
+  },
+  update: function (userId, dataset, fields, modifier) {
+    return true;
+  },
+  remove: function (userId, dataset) {
+    // You can only remove projects that you created and nobody has posted comments to.
+    //return project.owner === userId && commented(project) === 0;
+    return true;
+  }
+});
+
+
+addDataset = function(dataset, project) {
+    //If no time is associated with the comment, add the current time.
+    if (typeof dataset.posted === 'undefined') {
+        var date = new Date();
+        dataset.posted = date.getTime();
+    }
+    //Set the comment's foreign key to project.
+    dataset.projectId = project._id;
+    Datasets.insert(dataset);
+}
+
+    //If no time is associated with the comment, add the current time.
+editDataset = function(dataset, project) {
+   
+        var date = new Date();
+        editTime= date.getTime();
+    
+    //Set the comment's foreign key to project.
+  
+    Datasets.update( {_id: project._id}, {$set:{edited:editTime} } );
+}
